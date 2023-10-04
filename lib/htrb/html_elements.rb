5e556f6bb0cde8084113e5b2804253230a958e49
Christopher Perkins <Christopher.Perkins@null.net>
2023-10-04 12:09:32 -0400

    Reworked what elements are made. Only allows current HTML 5 tags

--- lib/htrb/html_elements.rb
+++ lib/htrb/html_elements.rb
@@ -12,21 +12,23 @@ module HTRB
   end
 
   TAGS = [
-    :a, :abbr, :address, :area, :article, :aside, :audio, :b, :bdi, :bdo,
-    :blockquote, :body, :br, :button, :canvas, :caption, :cite, :code, :col,
-    :colgroup, :command, :datalist, :dd, :del, :details, :dfn, :div, :dl, :dt,
-    :em, :embed, :fieldset, :figcaption, :figure, :footer, :form, :h1, :h2,
-    :h3, :h4, :h5, :h6, :head, :header, :hr, :html, :i, :iframe, :img, :input,
-    :ins, :kbd, :keygen, :label, :legend, :li, :main, :map, :mark, :menu, :meta,
-    :meter, :nav, :object, :ol, :optgroup, :option, :output, :p, :param, :pre,
-    :progress, :q, :rp, :rt, :ruby, :s, :samp, :section, :select, :small,
-    :script, :source, :span, :strong, :sub, :summary, :sup, :table, :tbody, :td,
-    :textarea, :tfoot, :th, :thead, :time, :title, :tr, :track, :u, :ul, :var,
-    :video, :wbr,
+    :a, :abbr, :address, :area, :article, :aside, :audio, :b, :base, :bdi,
+    :bdo, :blockquote, :body, :br, :button, :canvas, :caption, :cite, :code,
+    :col, :colgroup, :data, :datalist, :dd, :del, :details, :dfn, :dialog,
+    :div, :dl, :dt, :em, :embed, :fieldset, :figcaption, :figure, :footer,
+    :form, :h1, :h2, :h3, :h4, :h5, :h6, :head, :header, :hgroup, :hr, :html,
+    :i, :iframe, :img, :input, :ins, :kbd, :keygen, :label, :legend, :li,
+    :link, :main, :map,  :mark, :math, :menu, :meta, :meter, :nav, :noscript,
+    :object,  :ol, :optgroup, :option, :output, :p, :picture, :portal, :pre,
+    :progress, :q, :rp, :rt, :ruby, :s, :samp, :script, :search, :section,
+    :select, :slot, :small, :source, :span, :strong, :style, :sub, :summary,
+    :sup, :svg, :table, :tbody, :td, :template, :textarea, :tfoot, :th,
+    :thead, :time, :title, :tr, :track, :u, :ul, :var, :video, :wbr,
   ]
 
   SELF_CLOSING = [
-    :area, :base, :br, :col, :embed, :hr, :img, :input, :keygen, :link, :meta, :param, :source, :track, :wbr,
+    :area, :base, :br, :col, :embed, :hr, :img, :input, :keygen, :link, :meta,
+    :source, :track, :wbr,
   ]
 
   TAGS.each do |tag|
